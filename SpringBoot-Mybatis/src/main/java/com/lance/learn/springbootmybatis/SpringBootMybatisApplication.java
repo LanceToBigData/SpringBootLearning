7ed4ba2ed151d34c8f5033b3a7c0890339685617
ZhangYinHua <zhangyinhua@chinasie.com>
2018-08-02 13:14:23 +0800

    SpringBootLearning增加Mybatis集成模块搭建

---
+++ SpringBoot-Mybatis/src/main/java/com/lance/learn/springbootmybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.lance.learn.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisApplication.class, args);
+    }
+}
